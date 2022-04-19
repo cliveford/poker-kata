4d00663a1bab66d061aeba652a55f33d3358f5ed
cliveford <78969930+cliveford@users.noreply.github.com>
2022-04-19 14:46:24 +0100

    feat: Initial commit
    Set up project, changed language level to java 17, added .gitignore and README.md

---
+++ src/main/java/org/cliveford/poker/Poker.java
@@ -0,0 +1,10 @@
+package org.cliveford.poker;
+
+
+public class Poker
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/cliveford/poker/PokerTest.java
@@ -0,0 +1,16 @@
+package org.cliveford.poker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class PokerTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
